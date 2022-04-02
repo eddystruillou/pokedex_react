b57abf38056b6cf3a4c62e1323a1a05e698c0c4f
eddystruillou <eddy.s25@hotmail.fr>
2022-04-02 16:30:57 +0200

    Add Button component

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "../Button/Button";
+import "./Card.css";
+
+function Card() {
+    return (
+      <div className="Card">
+        <Button />
+      </div>
+    );
+}
+  
+export default Card;
