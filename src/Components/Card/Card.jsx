88956c35ac092ec6c41d7fde078d31d0ac8d9a6d
eddystruillou <eddy.s25@hotmail.fr>
2022-04-04 13:04:21 +0200

    Add Description component

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Button from "../Button/Button";
+import Description from "../Description/Description";
 import "./Card.css";
 
 function Card() {
     return (
       <div className="Card">
         <Button />
+        <Description />
       </div>
     );
 }
