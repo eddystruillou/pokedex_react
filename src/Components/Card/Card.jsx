10518fcd71e371fd9ef5bb0628242488de40f466
eddystruillou <eddy.s25@hotmail.fr>
2022-04-06 13:38:51 +0200

    Add call to the pokeapi at the start of the application

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "../Button/Button";
 import Description from "../Description/Description";
 import Picture from "../Picture/Picture";
 import "./Card.css";
 
 function Card() {
+  const [error, setError] = useState(null);
+  const [id, setId] = useState(1);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [item, setItem] = useState([]);
+
+  useEffect(() => {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${1}/`)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            console.log(result);
+            setIsLoaded(true);
+            setItem(result);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }, [])
+
     return (
       <div className="card">
-        <h2 className="title">Title of the pokemon</h2>
+        <h2 className="title">{item.name}</h2>
         <Picture />
         <Button />
         <Description />
