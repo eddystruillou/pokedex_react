c724ad95104300d9a5c8db81b7ef54a9dd5a4c49
eddystruillou <eddy.s25@hotmail.fr>
2022-04-12 13:27:23 +0200

    Add progressBar and update style in description component

--- src/Components/Description/Description.jsx
+++ src/Components/Description/Description.jsx
@@ -4,11 +4,15 @@ import "./Description.css";
 function Description({stats}) {
     return (
       <div className="description">
+        <hr/>
         <h3>Stats</h3>
         <ul>
           {stats.map(item =>
             <li key={item.base_stat + item.stat.name}>
-            <span>{item.stat.name}</span>: <span>{item.base_stat}</span>
+              <span className="statName">{item.stat.name}</span>
+              <span className="statValue">{item.base_stat}</span> 
+              <progress max="200" value={item.base_stat}>
+              </progress>
             </li>
           )}
         </ul>
