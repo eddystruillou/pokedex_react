3475ccbeb124315961db5fcd01ab7dfed89b2ece
eddystruillou <eddy.s25@hotmail.fr>
2022-06-16 18:27:48 +0200

    Add follow & unfollow state with icons

--- src/Components/Item/Item.tsx
+++ src/Components/Item/Item.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import unfollowIcon from './../../images/unfollow.png';
+import followIcon from './../../images/follow.png';
 import './Item.css';
 
 const Item: React.FC<{sprite: string, name: string}> = ({ sprite, name}) => {
+  const [follow, setFollow] = useState(false)
+
+  const handleFollowOrUnfollow = function () {
+    if(follow) {
+      setFollow(false)
+    } else {
+      setFollow(true)
+    }
+  }
+
   return (
     <div className='item'>
       <img src={sprite} alt="#" className="image" />
+      <img src={ follow ? followIcon : unfollowIcon} className="icon" onClick={handleFollowOrUnfollow} />
       <div className="title">{name}</div>
     </div>
   )
