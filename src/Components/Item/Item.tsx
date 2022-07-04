83d0e8b8997ae10fca437879abdd235b7a9e836c
eddystruillou <eddy.s25@hotmail.fr>
2022-07-04 22:44:49 +0200

    Add the possibility to follow or unfollow a pokemon in the store

--- src/Components/Item/Item.tsx
+++ src/Components/Item/Item.tsx
@@ -1,28 +1,28 @@
 import { useState } from 'react';
 import { PokemonFormatedData } from '../../model';
+import { useDispatch } from 'react-redux';
+import { followPokemon } from '../../features/pokemons/pokemons-slice';
+// Icons
 import unfollowIcon from './../../images/unfollow.png';
 import followIcon from './../../images/follow.png';
+// Style
 import './Item.css';
 
 const Item = ({ pokeData }: {pokeData: PokemonFormatedData}) => {
-  // State to know if the pokemon is followed or not
-  const [follow, setFollow] = useState(false);
+  // Update store
+  const dispatch = useDispatch();
 
   /**
    * Used to follow or unfollow a pokemon
    */
   const handleFollowOrUnfollow = () => {
-    if(follow) {
-      setFollow(false)
-    } else {
-      setFollow(true)
-    }
+    dispatch(followPokemon(pokeData.id));
   }
 
   return (
     <div className='item'>
       <img src={pokeData.img} alt="#" className="image" />
-      <img src={follow ? followIcon : unfollowIcon} className="icon" onClick={handleFollowOrUnfollow} />
+      <img src={pokeData.isFollow ? followIcon : unfollowIcon} className="icon" onClick={handleFollowOrUnfollow} />
       <div className="title">{pokeData.name}</div>
     </div>
   )
